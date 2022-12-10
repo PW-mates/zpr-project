3f5964f7f1aca22226ca2b4f7186b678a7685fa0
AndriiDemk <andriidemydenkow@gmail.com>
2022-12-10 17:42:14 +0100

    Add basic file structure, cmake with libssh

---
+++ src/main.cc
@@ -0,0 +1,14 @@
+//
+// Created by andrii on 10.12.22.
+//
+#include <iostream>
+#include <libssh/libssh.h>
+
+using namespace std;
+
+int main() {
+    ssh_session session;
+    ssh_channel channel;
+    cout << "test";
+    return 0;
+}
