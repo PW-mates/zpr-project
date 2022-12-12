3a4d76d83415e6e3a0feb8568110fe8d1c7c740f
Minh Nguyen Cong <clovers1254@gmail.com>
2022-12-12 22:00:08 +0100

    Create CI (#1)
    * Fix CI
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI
    
    * Update coverage
    
    * Update coverage
    
    * Update coverage
    
    * Update coverage
    
    * Update coverage

--- configure/linux.sh
+++ configure/linux.sh
@@ -4,3 +4,5 @@ sudo apt-get update
 sudo apt-get install libssh-dev
 sudo apt-get install libboost-all-dev
 sudo apt-get install libgtest-dev
+sudo apt-get install doxygen
+sudo apt-get install lcov
